7c5daf2fa5c9648a30db833dbbdb2f04b5e490f9
Uzo <uzo@MacBook-Pro-von-System.local>
2021-11-10 16:57:57 +0100

    added confgi file

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+  // Throw generic error
+  throw new Error("Couldn't find .env file");
+}
+
+export default {
+  /**
+   *  Application port.
+   */
+  port: process.env.PORT,
+
+  /**
+   * CockroachDB connection options.
+   */
+  database: {
+  },
+};
