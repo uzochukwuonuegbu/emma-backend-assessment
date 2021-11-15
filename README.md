a89278801b304b908d50c0a958a52902f3d26812
Uzo <uzo@MacBook-Pro-von-System.local>
2021-11-16 00:23:19 +0100

    finished building out the queries

---
+++ README.md
@@ -0,0 +1,21 @@
+Steps Taken To Build The Query:
+
+1. Get all merchants the use spend money on within a period (name: derived_table):
+- SELECT transactions.merchant_id AS merchant_id,
+SUM(transactions.amount) AS logged_in_user_amount_spent FROM transactions WHERE transactions.user_id='${user}' GROUP BY transactions.merchant_id
+
+2. Query transactions and sum other users amount spent of the merchants gotten from the first sub-query:
+- FROM (SELECT transactions.merchant_id, transactions.user_id, logged_in_user_amount_spent, SUM(transactions.amount) AS other_user_amount_spent FROM transactions, (derived_table) WHERE transactions.merchant_id=derived_table.merchant_id
+
+3. Calculate percentile rank for each merchant returned using the formular:
+- (no of users who spent less amount on each merchant) / (total number of users who spent on each merchant)
+- CAST(SUM(CASE WHEN logged_in_user_amount_spent > other_user_amount_spent THEN 1 ELSE 0 END) as FLOAT) / COUNT(DISTINCT user_id) AS percentile_rank
+
+
+How I ensured scalability:
+
+1. Avoided the use of window functions while building the queries.
+
+2. Setup indexes on user_id, merchant_id and date for transactions table, because it's a large table. So, queries are generally faster because we're reducing the time our database needs to locate the data for a the query.
+
+3. Avoided long running loops in the business logic layer, and used sub-queries instead.
--- src/repository/merchantRepository.ts
+++ src/repository/merchantRepository.ts
@@ -11,19 +11,23 @@ export default class MerchantRepository {
     }
 
     public async findAllUserMerchants(queryParams: QueryParams): Promise<any[]> {
-        // const { user, date } = queryParams;
+        const { user, date } = queryParams;
+        // date = 1636502400
         // const d = new Date();
         // const newDate = String(d.setMonth(d.getMonth()-1));
         // console.log({data:d});
-        const merchants = await this.dbInstance
-        .select('*')
-        // .sum('amount')
-        .from('transactions')
-        // .where('user_id', '6c84fb90-12c4-11e1-840d-7b25c5ee775a')
-        // .where('created_at', 1636718400)
-        // .innerJoin('merchants', 'transactions.merchant_id', 'merchants.id')
-        // group merchants by the amount spent
-        // use each amount(per merchant) to rank, considering the toal amount spent on
-        return merchants;
+        const { rows } = await this.dbInstance.raw(`
+        SELECT merchant_id, 
+        CAST(SUM(CASE WHEN logged_in_user_amount_spent > other_user_amount_spent THEN 1 ELSE 0 END) as FLOAT) / COUNT(DISTINCT user_id)
+        AS percentile_rank 
+        FROM (SELECT transactions.merchant_id, transactions.user_id, logged_in_user_amount_spent, SUM(transactions.amount)
+        AS other_user_amount_spent
+        FROM transactions, (SELECT transactions.merchant_id AS merchant_id, SUM(transactions.amount)
+        AS logged_in_user_amount_spent FROM transactions WHERE transactions.user_id='${user}' GROUP BY transactions.merchant_id) AS derived_table
+        WHERE transactions.merchant_id=derived_table.merchant_id
+        GROUP BY transactions.merchant_id, derived_table.logged_in_user_amount_spent, transactions.user_id) AS final_table
+        GROUP BY final_table.merchant_id
+        `)
+        return rows;
     }
 }
